47959c403797a3d95f7562e80bf4d33c42893ca4
Alisontristao <alisontristao@hotmail.com>
2023-02-27 14:01:54 -0300

    Biblioteca PID pronta - nao testada

---
+++ lib/PID/PID.cpp
@@ -0,0 +1,130 @@
+// autor Alison
+// email AlisonTristao@hotmail.com
+
+#include "PID.h"
+#include <Arduino.h>
+
+void
+PID::begin(double kpr, double kir, double kdr, int speed){
+    this->kpr = kpr; 
+    this->kir = kir; 
+    this->kdr = kdr; 
+    this->speed = speed;
+    verb = false;
+    lastTime = 0;
+    sumError = 0;
+}
+
+void
+PID::begin(double kpr, double kir, double kdr,
+            double kpt, double kit, double kdt, int speed){
+    this->kpr = kpr; 
+    this->kir = kir; 
+    this->kdr = kdr; 
+    this->kpt = kpt; 
+    this->kit = kit; 
+    this->kdt = kdt; 
+    this->speed = speed;
+    verb = false;
+    lastTime = 0;
+    sumError = 0;
+}
+
+void
+PID::setConst(double kpr, double kir, double kdr){
+    this->kpr = kpr; 
+    this->kir = kir; 
+    this->kdr = kdr; 
+}
+
+void
+PID::setConst(double kpr, double kir, double kdr,
+            double kpt, double kit, double kdt){
+    this->kpr = kpr; 
+    this->kir = kir; 
+    this->kdr = kdr; 
+    this->kpt = kpt; 
+    this->kit = kit; 
+    this->kdt = kdt; 
+}
+
+void
+PID::setSpeed(int speed){
+    this->speed = speed; 
+}
+
+void
+PID::setVerb(bool verb){
+    this->verb = verb; 
+}
+
+int32_t
+PID::pidEspecif(double kp, double ki, double kd, int32_t error){
+    // calcula quanto tempo passou desde o ultimo calculo
+    float t = (millis() - lastTime)/1000.0;
+    lastTime = millis();
+
+    // proporcional
+    double P = kp * error;
+    
+    // integral 
+    double I = (sumError * ki) * t;
+
+    // derivativa
+    double D = (lastError - error) * kd * t;
+
+    // garante que a integral nao sature
+    if((I >= speed) && (error >= 0)){
+        I = speed;
+    }else if((I <= -speed/2) && (error <= 0)){
+        I = -speed;
+    }else{
+        sumError += error;
+    }
+
+    // soma tudo 
+    int32_t PID = P + I + D;
+
+    // garante que o PID nao sature
+    if(PID > speed){
+        PID = speed;
+    }else if(PID < -speed){
+        PID = -speed;
+    }
+
+    if(verb){
+        Serial.printf("Tempo: %f \t", t);
+        Serial.printf("P: %f \t I: %f, \t D: %f \t", P, I, D);
+        Serial.printf("PID: %d \t", PID);
+        Serial.printf("Erro somado: %d \t Erro passado: %d, \t Erro: %d \n", sumError, lastError, error);
+    }
+
+    // salva o ultimo erro
+    lastError = error;
+
+    return PID;
+}
+
+int32_t 
+PID::calcPID(int32_t error){
+    return pidEspecif(kpr, kir, kdr, error);
+}
+
+int32_t 
+PID::calcPID(int32_t error, uint8_t mode){
+    switch (mode)
+    {
+    case ROTACIONAL:
+        return pidEspecif(kpr, kir, kdr, error);
+        break;
+    case TRANSLACIONAL:
+        return pidEspecif(kpt, kit, kdt, error);
+        break;
+    
+    default:
+        Serial.println("Modo não encontrado!");
+        break;
+    }
+}
+
+
---
+++ lib/PID/PID.h
@@ -0,0 +1,41 @@
+// autor Alison
+// email AlisonTristao@hotmail.com
+
+#pragma once // substitui o indef endif
+#include <cstdint>
+#include <Arduino.h>
+
+class PID{
+    public:
+        // inicia as constantes apenas rotacionais
+        void begin(double kpr, double kir, double kdr, int speed);
+        // inicia com constantes tambem translacionais
+        void begin(double kpr, double kir, double kdr,
+                    double kpt, double kit, double kdt, int speed);
+        // atualiza as constantes rotacionais
+        void setConst(double kpr, double kir, double kdr);
+        // atualiza todas constantes
+        void setConst(double kpr, double kir, double kdr,
+                    double kpt, double kit, double kdt); 
+        // atualiza o valor da velociade
+        void setSpeed(int speed);
+        void setVerb(bool verb);
+        // calcula o PID rotacional - default
+        int32_t calcPID(int32_t error);
+        // escolhemos qual modo do PID usar 
+        int32_t calcPID(int32_t error, uint8_t mode);
+    private:
+        // calculo do PID geral
+        int pidEspecif(double kp, double ki, double kd, int32_t error);
+        // converte o PID para PWM - ainda precisamos pensar sobre
+        int transformForPWM(int PID);
+        uint32_t lastTime;
+        double kpt, kit, kdt, kpr, kir, kdr;
+        int32_t speed, sumError, lastError;
+        bool verb;
+};
+
+typedef enum {
+    ROTACIONAL,
+    TRANSLACIONAL
+} modos;
