f1f57189129a8a4654e99d3a250e781af299df8a
Alisontristao <alisontristao@hotmail.com>
2023-03-05 15:38:28 -0300

    test modelo matematico teoria

---
+++ test/main.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <cmath>
+
+#define p1 15
+#define p2 4
+
+const double pi = cos(-1);
+double tempo, 
+       omega,
+       psi, 
+       distan, 
+       vel, 
+       vx, 
+       vy, 
+       v1[2], 
+       v2[2], 
+       v1g[2], 
+       v2g[2], 
+       r = 4.0, 
+       p, 
+       d = 3, 
+       d1 = -2, 
+       d2 = 2;
+
+float grausForRad(int8_t graus){
+    return (graus*3.14159265)/180.0;
+}
+
+void setup(){
+    Serial.begin(115200);
+}
+
+void loop(){
+    int8_t tempo = map(analogRead(p1), 0, 4095, 0, 100);
+    int8_t angulo = map(analogRead(p2), 0, 4095, -90, 90);
+
+    p = tan(grausForRad(angulo)) * d;
+    psi = (2*p)/((p*p) + (d*d));
+    distan = sqrt(p*p + d*d);
+    omega = grausForRad(angulo)/tempo;
+    vel = distan/tempo;
+    vx = sin(grausForRad(angulo)) * vel;
+    vy = cos(grausForRad(angulo)) * vel;
+
+    v1[0] = vx + (d1 * omega * cos(grausForRad(angulo)));
+    v1[1] = vy + (d1 * omega * sin(grausForRad(angulo)));
+
+    v2[0] = vx + (d2 * omega * cos(grausForRad(angulo)));
+    v2[1] = vy + (d2 * omega * sin(grausForRad(angulo)));
+
+    v1g[0] = vx * (1 + (r * psi));
+    v1g[1] = vy * (1 + (r * psi));
+
+    v2g[0] = vx * (1 - (r * psi));
+    v2g[1] = vy * (1 - (r * psi));
+
+    Serial.printf("t: %03ds\tpeta: %03d°\tw: %.3frad/s\t (cm/s) -> vx: %.3f\tvy: %.3f\t", 
+    tempo, (angulo), omega, vx, vy);
+
+    /*Serial.printf("v1:{%.3f, %.3f} \t v2:{%.3f, %.3f}\tv1:{%.3f, %.3f}\tv2:{%.3f, %.3f}\n", 
+    v1[0], v1[1], v2[0], v2[1], v1g[0], v1g[1], v2g[0], v2g[1]);*/
+
+    //Serial.printf("%03d\t%03d\t", tempo, (angulo));
+
+    Serial.printf("v1: %.3f, %.3f \tv2: %.3f, %.3f\tv1: %.3f, %.3f\tv2: %.3f, %.3f\n", 
+    v1[0], v1[1], v2[0], v2[1], v1g[0], v1g[1], v2g[0], v2g[1]);
+
+    delay(250);
+}
