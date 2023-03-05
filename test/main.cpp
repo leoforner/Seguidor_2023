4624c7418b8cec7e09b2432e41f8077c2bad4f05
Alisontristao <alisontristao@hotmail.com>
2023-03-05 19:22:34 -0300

    test quase pronto

--- test/main.cpp
+++ test/main.cpp
@@ -20,7 +20,13 @@ double tempo,
        p, 
        d = 3, 
        d1 = -2, 
-       d2 = 2;
+       d2 = 2, 
+       ve1,
+       vd1,
+       ve2,
+       vd2,
+       phi1, 
+       phi2;
 
 float grausForRad(int8_t graus){
     return (graus*3.14159265)/180.0;
@@ -54,16 +60,27 @@ void loop(){
     v2g[0] = vx * (1 - (r * psi));
     v2g[1] = vy * (1 - (r * psi));
 
-    Serial.printf("t: %03ds\tpeta: %03d°\tw: %.3frad/s\t (cm/s) -> vx: %.3f\tvy: %.3f\t", 
+    /*Serial.printf("t: %03ds\tpeta: %03d°\tw: %.3frad/s\t (cm/s) -> vx: %.3f\tvy: %.3f\t", 
     tempo, (angulo), omega, vx, vy);
 
-    /*Serial.printf("v1:{%.3f, %.3f} \t v2:{%.3f, %.3f}\tv1:{%.3f, %.3f}\tv2:{%.3f, %.3f}\n", 
+    Serial.printf("v1:{%.3f, %.3f} \t v2:{%.3f, %.3f}\tv1:{%.3f, %.3f}\tv2:{%.3f, %.3f}\n", 
     v1[0], v1[1], v2[0], v2[1], v1g[0], v1g[1], v2g[0], v2g[1]);*/
 
-    //Serial.printf("%03d\t%03d\t", tempo, (angulo));
+    Serial.printf("%03d\t%03d\t", tempo, (angulo));
 
-    Serial.printf("v1: %.3f, %.3f \tv2: %.3f, %.3f\tv1: %.3f, %.3f\tv2: %.3f, %.3f\n", 
-    v1[0], v1[1], v2[0], v2[1], v1g[0], v1g[1], v2g[0], v2g[1]);
+    ve1 = sqrt(v1[0]*v1[0] + v1[1]*v1[1]);
+    vd1 = sqrt(v2[0]*v2[0] + v2[1]*v2[1]);
+
+    ve2 = sqrt(v1g[0]*v1g[0] + v1g[1]*v1g[1]);
+    vd2 = sqrt(v2g[0]*v2g[0] + v2g[1]*v2g[1]);  
+
+    Serial.printf("v1: %.3f, %.3f \tv2: %.3f, %.3f\tv1: %.3f, %.3f\tv2: %.3f, %.3f\t%.3f\t%.3f\t%.3f\t%.3f\t", 
+    v1[0], v1[1], v2[0], v2[1], v1g[0], v1g[1], v2g[0], v2g[1], ve1, vd1, ve2, vd2);
+
+    phi1 = 1.57 - atan2(r, (vd1 - ve1));
+    phi2 = atan2((vd2 - ve2), r) - 1.57;
+
+    Serial.printf("r1: %.3f\tr2: %.3f\n", ve1/sin(phi1), ve2/sin(phi2));
 
     delay(250);
 }
