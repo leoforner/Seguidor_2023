09d7a0a2f5eb0882ed1b01e50051e276de7524af
AlisonTristao <AlisonTristao@hotmail.com>
2023-04-23 19:54:43 -0300

    testes divisor de tensao e regressao linar GPIO

--- test/regressão linear GPIO_esp32/calcula_tensao_bateria.cpp
+++ test/regressão linear GPIO_esp32/calcula_tensao_bateria.cpp
@@ -3,14 +3,14 @@
 #define in 33
 
 double converteAnalogParaTensao(int analog){
-    if(analog > 1030 && analog < 2700){
+    if(analog > 1030 && analog < 2700){ // faixa precisa
         double reg = 
                 0.0000000788*(pow(analog, 3)) - // x³
                 0.0003832376*(pow(analog, 2)) + // x²
                 1.5860951797*analog +           // x
                 28.787348508;
 
-        return reg*7/2225;
+        return reg*7/2225; // regra de 3 para converter para 7v
     }else{
         Serial.println("tensão fora da margem precisa da esp32.");
         return 0;
@@ -24,6 +24,7 @@ void setup() {
 
 void loop() {
     double tensao = 0;
+    // calcula a media de 100 medidas
     for(int i = 0; i < 100; i++){
         int analog = analogRead(in);
         tensao += converteAnalogParaTensao(analog);
