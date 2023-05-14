751fecb0a0d0e8a60938cf42e5ef6040705fbf07
AlisonTristao <AlisonTristao@hotmail.com>
2023-05-14 08:50:07 -0300

    mudei nada

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <EnconderCounter.h>
+
+EnconderCounter encoder(23, PCNT_UNIT_0, 100, 1000);
 
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
-
+    
 }
