f7155693bcfdf60ecfc44873e21a5b93d39ccbad
AlisonTristao <AlisonTristao@hotmail.com>
2023-05-15 21:59:35 -0300

    netoflix bugado

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include <Arduino.h>
-#include <EnconderCounter.h>
-
-EnconderCounter encoder(23, PCNT_UNIT_0, 100);
 
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
-    Serial.println(encoder.getPulses());
-    delay(100);
+
 }
