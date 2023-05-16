987395baab1266035e83b3063084a1d143a6ff97
Nettoflix <netto2002.sjb@gmail.com>
2023-05-15 22:16:40 -0300

    Revert "netoflix bugado"
    This reverts commit f7155693bcfdf60ecfc44873e21a5b93d39ccbad.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <EnconderCounter.h>
+
+EnconderCounter encoder(23, PCNT_UNIT_0, 100);
 
 void setup() {
     Serial.begin(115200);
@@ -14,5 +17,6 @@ void setup() {
 }
 
 void loop() {
-
+    Serial.println(encoder.getPulses());
+    delay(100);
 }
