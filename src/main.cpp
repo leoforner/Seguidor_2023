ff60fcd57cea71d99cddb1a28306e48223fea3d9
MeJustus <124215222+MeJustus@users.noreply.github.com>
2023-05-17 22:47:33 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+
+BluetoothSerial SerialBt;
+
+float kp1, ki1, kd1, kp2, ki2, kd2, kp3, ki3, kd3, speedR, speedL;
 
 void setup() {
     Serial.begin(115200);
-
-    Serial.println("espera");
-    delay(3000);
-
-    ls.begin();
-    ls.setLed(2);
-    ls.setVerb(true);
-    ls.calibration(STATIC);
-    ls.printConfig();
+    SerialBt.begin("esp32");
 }
 
 void loop() {
-    Serial.println(encoder.getPulses());
-    delay(100);
+    String texto;
+    char a;
+
+    //recebe o sinal bluetooth e transforma em string
+    while(SerialBt.available()){
+      a = SerialBt.read();
+      texto += a;    
+    }            
+    
+    //transforma nas variáveis corretas dependendo da chave recebida
+      if(texto[0] == '{' && texto[(texto.length()-1)] == '}'){
+        sscanf(texto.c_str(), "{%f/%f/%f/%f/%f/%f/%f/%f/%f/}", &kp1, &ki1, &kd1, &kp2, &ki2, &kd2, &kp3, &ki3, &kd3);
+      }
+      else if(texto[0] == '[' && texto[(texto.length()-1)] == ']'){
+        sscanf(texto.c_str(), "[%f/%f]", &speedR, &speedL);
+      }
+      //reconhece "quebras" no sinal bluetooth
+      else if(!texto.isEmpty()){
+        Serial.println("ERRO");
+      }
 }
