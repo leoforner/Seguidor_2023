7c1562c43a210339d6edc3392bfc2440f6cd447e
AlisonTristao <AlisonTristao@hotmail.com>
2023-05-20 19:27:12 -0300

    testes velocidade do carrinho

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,71 @@
 #include <Arduino.h>
+#include <EnconderCounter.h>
+EnconderCounter encoder(23, PCNT_UNIT_0, 140, 1000);
 
-void setup() {
-    Serial.begin(115200);
+// divisor tensao 
+#define in 36
+// bateria 
+#define pwmb 5
+#define pwma 18
+
+double medidas[300]; 
+
+
+float interpolacaoLinear(float analog, float vMin, float vMax,
+                float adc_min, float adc_max) {
+  return vMin +
+         (analog - adc_min) * (vMax - vMin) / (adc_max - adc_min);
 }
 
+float analogicoParaTensao(float analog) {
+  if (analog < 22)
+    return interpolacaoLinear(analog, 0.0, 0.17, 0, 22);
+  else if (analog >= 22 && analog < 1832)
+    return interpolacaoLinear(analog, 0.17, 1.66, 22, 1832);
+  else if (analog >= 1832 && analog < 3138)
+    return interpolacaoLinear(analog, 1.66, 2.69, 1832, 3138);
+  else if (analog >= 3138 && analog < 4095)
+    return interpolacaoLinear(analog, 2.69, 3.12, 3138, 4095);
+  else
+    return 3.2;
+}
+
+void setup() {
+    delay(3000);
+    Serial.begin(115200);
+    pinMode(pwmb, OUTPUT);
+    pinMode(pwma, OUTPUT);
+    pinMode(in, INPUT);
+
+    ledcSetup(0, 5000, 12);
+    ledcAttachPin(pwmb, 0); 
+    ledcSetup(1, 5000, 12);
+    ledcAttachPin(pwma, 1);  
+}
+
+bool dale = false;
 void loop() {
-    Serial.println("netto?");
-    //tomatomatoma 
+    // aplicar pwm duarante tempo determinado
+    for(int i = 0; i < 200; i++){
+        float x = analogRead(in);
+        x = (analogicoParaTensao(x))*4.03;
+        float saida = (4.0*4095)/x;
+
+        ledcWrite(0, saida);
+        ledcWrite(1, saida);
+        medidas[i] = encoder.getRPS();
+        delay(10);
+        Serial.println(x);
+    }
+    dale = true;
+    ledcWrite(0, 0);
+    ledcWrite(1, 0);
+
+    while(dale){
+      Serial.println("-");
+        for(int i = 0; i < 200; i++){
+            Serial.printf("%.3f;\n", medidas[i]);
+        }
+        delay(2000);
+    }
 }
