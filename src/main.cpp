5f52d563a9ad9d17e5215c7b6ca0be4bdbe778ac
Nettoflix <netto2002.sjb@gmail.com>
2023-05-24 22:01:02 -0300

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
+#define pwmb 5
 
 void setup() {
     Serial.begin(115200);
+    ledcSetup(0, 5000, 12);
+    ledcAttachPin(pwmb, 0);
 }
 
 void loop() {
     Serial.println("netto?");
+    ledcWrite(0, 4095);
+
     //tomatomatoma 
 }
