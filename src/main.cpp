f245d8729c8095dc397eec275731005062d9c714
Joao Queiroz <jvmdq1@gmail.com>
2023-08-17 17:30:27 -0300

    alteração com codigo base e envio pela ESPNOW

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,86 @@
 // bibliotecas
-#include <BluetoothSerial.h>
-#include <lineSensor.h>
 #include <Arduino.h>
-#include <PD.h>
+#include <EnconderCounter.h>
 
 // includes
 #include "adc.h"
 #include "pins.h"
-#include "times.h"
 #include "wheels.h"
-#include "exceptions.h"
+#include <esp_now.h>
+#include <WiFi.h>
 
-// SerialBT bluetooth
-BluetoothSerial SerialBT;
-double position;
-int32_t pid = 0;
-uint32_t speed = 2800;
+EncoderCounter encoderLeft(enc3, PCNT_UNIT_0, 140, 1000);
+EncoderCounter encoderRight(enc1, PCNT_UNIT_1, 140, 1000);
 
-// valores maximos dos sensores laterais
-uint16_t whiteLeft = 0, whiteRight = 0;
-
-uint8_t contadorIntersec = 0;
-
-// constantes do PID
-double kp = 400, ki = 10, kd = 45;
-
-// inicia o estado do carrinho como off
-stt state = OFF;
-
-// sensores frontais (defina o carrinho em pins.h)
-#ifdef FRANK
-    uint8_t pins[] = {13, 12, 14, 27, 26, 25, 33, 32}, pinCount = 8;
-    // {32, 33, 25, 26, 27, 14, 12, 13}
-    // {13, 12, 14, 27, 26, 25, 33, 32}
-#endif
-
-#ifdef FOMINHA
-    uint8_t pins[] = {35, 32, 33, 25, 26, 27, 14, 12}, pinCount = 8;
-#endif
-
-// sensor frontal
-lineSensor forwardSensor(pinCount, pins, true);             
-
-// controle do carrinho (usamos apenas simplePID)
-PD control(1, 1, 1, 1);
-
+         
 // rodas
 wheels wheelLeft, wheelRight;
 
 // canais pwm (precisa definir aqui para definir em pins.h)
 uint8_t channelLeft = 0, channelRight = 1;
 
+//declaracao de variaveis que serao enviadas para comunicação
+long int timestart = 0;
+long int DataTime[300];
+double DataVelesq[300];
+double DataVeldir[300];
+
+// MAC Address do ESP Receptor
+uint8_t broadcastAddress[] = {0x84, 0xCC, 0xA8, 0x54, 0x53, 0x30};
+
+// Definição da estrutura
+typedef struct struct_message {
+  long int time;
+  double velesq;
+  double veldir;
+  } struct_message;
+
+// Instancia da estrutura
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
+// confirmação de envio dos dados
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+int contador = 0; //iterador do loop de envio de dados
+
+
+
+
+
 void setup(){
-    Serial.begin(115200);
+   
     definePins();
-    SerialBT.begin("FOMINHA");
+     // Init Serial Monitor
+    Serial.begin(115200);
+    
+    // Set device as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(OnDataSent);
+    
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+    
+  // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
+  }
+    timestart = millis();
 
     // define as propriedades das rodas
     wheelLeft.mov = STOPPED;    // inicia como parada
@@ -71,56 +97,7 @@ void setup(){
     applyPWM(&wheelLeft, 0);
     applyPWM(&wheelRight, 0);
 
-    // anexa as interrupcoes ao segundo nucleo
-    xTaskCreatePinnedToCore(
-        interrupt,
-        "interrupt",
-        2048,
-        NULL,
-        2,
-        NULL,
-        0
-    );
-
-    // espera algum dispositivo conectar
-    while(state < CONNECT) {
-        for(uint8_t i = 0; i < 4; i++){
-            digitalWrite(led, HIGH);
-            delay(100);
-            digitalWrite(led, LOW);
-            delay(100);
-        }
-        delay(1000);
-    }
-
-    SerialBT.println("Carrinho ligado, pressione o botao para iniciar calibração do sensor frontal");
-    while(state < 1) delay(10);
-
-    // calibra o sensor frontal
-    SerialBT.println("Calibrando...");
-    forwardSensor.begin();
-    forwardSensor.setLed(led);
-    forwardSensor.calibration(STATIC);
-
-    SerialBT.println("Pressione o botao para iniciar calibração dos sensores laterais");
-    while(state < 2) delay(10);
-
-    // calibra os sensores laterias
-    uint32_t time = millis(); 
-    while((millis() - time) < 1500){
-        if(analogRead(left) > whiteLeft)   whiteLeft = analogRead(left);
-        if(analogRead(right) > whiteRight) whiteRight = analogRead(right);  
-    }
-    
-    digitalWrite(led, LOW);
-
-    SerialBT.println("Sensores calibrados, pressione o botao para iniciar trajeto");
-    while(state < 3) delay(10);
-
-    // debug
-    SerialBT.printf("kp: %.3f\nki: %.3f\nkd: %.3f\nspeed: %d\n", kp, ki, kd, speed);
-    SerialBT.println((analogicoParaTensao(analogRead(divTensao)))*3.96); // 7.6/1.92 7.6v viram 1.92v (divisor de tensão));
-
+    delay(3000);
     // sinalização piscando led
     for(uint8_t i = 0; i < 4; i++){
         digitalWrite(led, HIGH);
@@ -131,51 +108,40 @@ void setup(){
 }
 
 void loop(){
-    // final da pista
-    if(state == FINAL || state == OFF){
-        // freia as rodas para parar inercia do carrinho
-        brake(&wheelLeft);
-        brake(&wheelRight);
-        return;
-    }
-
-    // calcula a posição da linha (pinCout * 1000)/2 = 3500 (index 0 nao soma em search line)
-    position = (forwardSensor.searchLine(&state) - 3500)/100;
-
-    // calcula o pid
-    pid = control.simplePID(kp, ki, kd, position, 2*speed);
-
-    // velocidade toral das rodas
-    int16_t velLeft = speed;
-    int16_t velRight = speed;
-
-    // aplica o pid nas rodas
-    if(pid > 0) velRight = speed - pid;
-    else        velLeft = speed + pid;
-
-    /*acabamos tirando pois estava diminuindo o torque das rodas e 
-    / o robo nao conseguia fazer curvas fechadas apos retas longas*/
-
-    // calcula pwm max (correspondente a 6v)
-    /*float tensaoBateria = (analogicoParaTensao(analogRead(divTensao)))* 3.96; // 7.6/1.92; //7.6v viram 1.92v (divisor de tensão)
+    float tensaoBateria = (analogicoParaTensao(analogRead(divTensao)))* 3.96; // 7.6/1.92; //7.6v viram 1.92v (divisor de tensão)
     if(tensaoBateria < 7.7) state == OFF; // desliga
-    int pwm_6volts = (8.0*4095)/tensaoBateria;
-    // if(pwm_6volts > 4095) pwm_6volts = 4095; (na fonte de bancada para testar, use isso)
+    int pwm_saida = (8.0*4095)/tensaoBateria;
 
-    // pwm correspondente ao ponto que a roda nao gira
-    uint8_t pMorto = 0.2; // 20% do pwm maximo
-
-    // mapeia o pwm resultante para a faixa de pwm q queremos 
-    if(velRight >= 0)   velRight = map(velRight, 0, speed, pMorto*pwm_6volts, pwm_6volts);
-    else                velRight = map(velRight, -speed, 0, -pwm_6volts, -pMorto*pwm_6volts);
-
-    if(velLeft >= 0)    velLeft = map(velLeft, 0, speed, pMorto*pwm_6volts, pwm_6volts);
-    else                velLeft = map(velLeft, -speed, 0, -pwm_6volts, pMorto*pwm_6volts*-1);*/
-
+    
 
     // aplica o pwm
-    applyPWM(&wheelRight, velRight);
-    applyPWM(&wheelLeft, velLeft);  
+    applyPWM(&wheelRight, pwm_saida);
+    applyPWM(&wheelLeft, pwm_saida);  
+    // Set values to send
+  if (contador<300){
+    DataTime[contador] = millis() - timestart;
+    DataVelesq[contador] = random(-20, 20);
+    DataVeldir[contador] = random(-20, 20);
+  }else{
+    for (int i =0; i<300; i++){
+      myData.time = DataTime[i];
+      myData.velesq = DataVelesq[i];
+      myData.veldir = DataVeldir[i];
+      // Send message via ESP-NOW
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+     if (result == ESP_OK) {
+       Serial.println("Sent with success");
+     }
+     else {
+        Serial.println("Error sending the data");
+     }
+     delay(1000);
+    }
+
+  }
+  delay(14);
+  contador++;
 
     delay(2);
 }
